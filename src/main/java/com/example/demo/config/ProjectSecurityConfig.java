a29a323c01c51adec3bc33c21ac968515e50b0e5
Yuriy Yefremov <megadethov@gmail.com>
2022-01-30 14:34:23 +0200

    WebSecurityConfigurerAdapter - custom config

--- src/main/java/com/example/demo/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/demo/config/ProjectSecurityConfig.java
@@ -14,8 +14,30 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
          * Default configurations which will secure all the requests
          */
 
+/*
         http.authorizeRequests()
                 .anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+*/
+
+
+        /**
+         * Custom configurations as per our requirement
+         *      /my-account - Secured
+         *      /my-balance - Secured
+         *      /my-loans - Secured
+         *      /my-cards - Secured
+         *      /notices - Not Secured
+         *      /contact - Not Secured
+         */
+        http.authorizeRequests()
+                .antMatchers("/my-account").authenticated()
+                .antMatchers("/my-balance").authenticated()
+                .antMatchers("/my-loans").authenticated()
+                .antMatchers("/my-cards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
 
