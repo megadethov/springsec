863de7f15adacac753f27da87bd1fbbf82b49a86
Yuriy Yefremov <megadethov@gmail.com>
2022-01-30 13:47:20 +0200

    @ComponentScan added

--- src/main/java/com/example/demo/app/DemoApplication.java
+++ src/main/java/com/example/demo/app/DemoApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.example.demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.demo.controller")
 public class DemoApplication {
 
 	public static void main(String[] args) {
